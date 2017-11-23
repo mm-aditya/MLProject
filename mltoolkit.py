b46a0275e7d61a54401b8029aa44ee2eabab3f50
Aditya Manikashetti <aditya.manikashetti@gmail.com>
2017-11-23 15:34:32 +0800

    Minor Changes

--- mltoolkit.py
+++ mltoolkit.py
@@ -4,6 +4,7 @@ from parse import parseFileInput
 from pprint import pprint
 from collections import Counter
 import os
+import itertools
 
 
 def count(tokens, tags, tag, token):
@@ -12,6 +13,34 @@ def count(tokens, tags, tag, token):
         if (tokens[i] == token) and (tags[i] == tag):
             ctr=ctr+1
     return ctr
+
+
+def countc(tags1, state2, state1):
+    tags = tags1[:]
+    for i in range (len(tags)):
+        if tags[i] == None:
+            tags[i] = '#SPACE#'
+
+    if state2 == 'STOP':
+        state2 = '#SPACE#'
+    if state1 == 'START':
+        state1 = '#SPACE'
+
+    if state1 == 'START' and state2 == 'STOP':
+        return 0
+
+    ctr = 0
+    prev = None
+    for i in tags:
+        if prev==None:
+            prev = i
+        else:
+            if i==state2:
+                if prev==state1:
+                    ctr+=1
+        prev = i
+    return ctr
+
 
 
 def calculateEmission(tokens, tags, k=0):
@@ -41,7 +70,87 @@ def calculateEmission(tokens, tags, k=0):
         percent = float(ctr/lenz) * 100
         print(percent , "%" )            #Print percentage of emission parameters calculated
 
+    #pprint(emissionParams)
+
     return emissionParams
+
+
+
+def calculateTransmission(tags):
+    transmissionParams = {}
+
+    tagset = set(tags)
+    tagset.remove(None)
+    tagset.add('STOP')
+    # pprint(tagset)    
+
+    tagset2 = set(tags)
+    tagset2.remove(None)
+    tagset2.add('START')
+
+    for i in tagset:
+        temp = {}
+        for p in tagset2:
+            temp.update({p:None})
+        transmissionParams.update({i : temp})
+
+    for i in transmissionParams:
+        for p in transmissionParams[i]:
+            #print(i)
+            ctr = i
+            if i == 'START' or i=='STOP':
+                ctr = None
+            transmissionParams[i][p] = float(countc(tags,i,p)/float(tags.count(ctr)))
+            # pprint(tags)
+
+    #pprint(transmissionParams)
+
+    # for i in range (len(tokens)):
+    #     if tokens[i] == None:
+
+    return transmissionParams
+
+
+
+def viterbi(tags, tokens, observation):
+    #emission[i][j]
+    emission = calculateEmission(tokens, tags, 3)
+    transmission = calculateTransmission(tags)
+
+    tagset = set(tags)
+    tagset.remove(None)
+    tagset=list(tagset)
+    
+    viterbiPath = []
+    backpointer = []
+
+    for i in range (len(tagset)):
+        temp = []
+        temp.append(transmission[tagset[i]]['START'] * emission[tokens[0]][tagset[i]])
+        viterbiPath.append(temp)
+        temp = []
+        temp.append(0)
+        backpointer.append(temp)
+
+    pprint(viterbiPath)
+    pprint(backpointer)
+    print("\n")
+
+    # for i in range (1,len(observation)):
+    #     for p in range (len(tagset)):
+    #         viterbiPath[p].append()
+
+
+def maximizer(tagset, viterbiO, transmissionO, emissionO, time):
+    viterbi = viterbiO[:]
+    transmission = transmissionO.copy()
+    emission = emissionO.copy()
+
+    values = []
+
+    # for i in range (len(tagset)):
+    #     values.append()
+
 
 
 
@@ -85,10 +194,20 @@ def writeout(tokens, tags, filename):
 
 
 
-
 def main():
-    simpleSentiment('/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/train','/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/dev.in','/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/dev.out')
+    # simpleSentiment('/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/train','/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/dev.in','/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/dev.out')
 
+    temp = parseFile('/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/train')
+    tokens = temp[0]
+    tags = temp[1]
+
+    # calculateTransmission(tags)
+    # calculateEmission(tokens, tags, 3)
+    # viterbi(tags, tokens)
+
+
+    #Run this one for question 2
+    simpleSentiment('/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/train', '/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/dev.in', '/Users/aditya/Desktop/Machine Learning/Project/MLProject/EN/EN/dev.out')
 
 
 main()
